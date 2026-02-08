29b8ae39a0cee59bd9f47e2f0c2d500fc5edcf27
Nilay Basak <nilaybasak000@gmail.com>
2026-02-08 23:09:30 +0530

    feat: Implement Landing Page UI and Reusable Modal Component

---
+++ Frontend/src/utils/data.ts
@@ -0,0 +1,38 @@
+export interface Feature {
+  id: string;
+  title: string;
+  description: string;
+}
+
+export const APP_FEATURES: Feature[] = [
+  {
+    id: "01",
+    title: "Tailored Just for You",
+    description:
+      "Get Interview Questions and Model Answers Based on Your Role, Experience, and Specific Focus Areas -- No Generic Practice Here.",
+  },
+  {
+    id: "02",
+    title: "Learn at Your Own Pace",
+    description:
+      "Expand Answers Only When You're Ready to Deepen Your Understanding. Deep Dive into Any Concept Instantly with AI-Powered Detailed Explanations.",
+  },
+  {
+    id: "03",
+    title: "Capture Your Insights",
+    description:
+      "Add Personal Notes to Any Question and Pin Important Ones to the Top -- Making Learning Easier, Effective and More Organized.",
+  },
+  {
+    id: "04",
+    title: `Understand The "Why" Behind Answers`,
+    description:
+      "Beyond Just Answers -- Unlock Detailed Concept Explanations for a Better Understanding of the Topic Using AI.",
+  },
+  {
+    id: "05",
+    title: "Save, Organize, and Revisit",
+    description:
+      "Easily Save Your Interview Sets, Organize Them Neatly in Your Dashboard, and Pick Up Your Preparation Right Where You Left Off.",
+  },
+];
