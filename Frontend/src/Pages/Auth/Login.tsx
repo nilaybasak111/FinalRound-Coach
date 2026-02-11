f60815cee7c6a02254832a8b84f7d3a147b97004
Nilay Basak <nilaybasak000@gmail.com>
2026-02-11 12:18:51 +0530

    feat(auth): Integrate Axios Instance in Login Flow

--- Frontend/src/Pages/Auth/Login.tsx
+++ Frontend/src/Pages/Auth/Login.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Login = ({ setCurrentPage }: any) => {
   const [email, setEmail] = useState("");
@@ -28,6 +30,18 @@ const Login = ({ setCurrentPage }: any) => {
 
     // LogIn API Call
     try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token } = response.data;
+
+      // Set Token In Local Storage
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
     } catch (error: any) {
       if (error.response && error.response.data.message) {
         setError(error.response.data.message);
